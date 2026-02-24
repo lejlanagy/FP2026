9e48c552befcfed7b15b7cc4ea8e9ef5c415d426
Lejla <lejla.nagy@gmail.com>
2026-02-24 14:10:51 +0200

    lab1 done, lab2 almost

--- 01.labor/lab1_A.hs
+++ 01.labor/lab1_A.hs
@@ -66,3 +66,69 @@ masodF a b c
         delta = b^2 - 4*a*c
         gy1 = (-b + sqrt delta ) / (2*a)
         gy2 = (-b - sqrt delta ) / (2*a)
+
+--negyzetgyok - elso N db termeszetes szamra
+negyzetgyokN n = [sqrt i | i <- [ 1 .. n ]]
+
+negyzetN n = [i * i | i <- [1 .. n]]
+
+--koboles
+kobN n = [i ^ 3 | i <- [1 .. n]]
+
+--nem negyzetszamok
+nemNegyzetN n = [i | i <- [1 .. n], (sqrt i * sqrt i) /= i]
+
+xhatvanyN x n = [x ^ i | i <- [1 .. n]]
+
+osztokN n = [i | i <- [1 .. n], n `mod` i == 0, mod i 2 == 0]
+osztokN2 n = [i | i <- [2,4 .. n], mod n i == 0]
+
+osztok n = [i | i <- [1..n], mod n i == 0]
+primszam n = osztok n == [1,n]
+
+primszamokN n = [i | i <- [2..n], primszam i]
+
+primszamokN2 n = [i | i <- [2..n], primszam i]
+    where 
+        primszamL n = osztokL n == [1,n]
+        osztokL n = [i | i <- [1 .. n], mod n i == 0]
+
+-- osszetett szamok listaja
+--ami nem primszam
+osszetettN n = [i | i <- [0..n], not(primszam i)]
+
+--paratlan osszetett szamok listaja
+paratlanOsszetettN n = [i | i <- [0..n], not(primszam i), mod i 2 /= 0]
+paratlanOsszetettN2 n = [i | i <- [3,5..n], not(primszam i)]
+
+--n-nel kisebb pitagoraszi szamharmasok
+pitagorasz n = [(a,b,c) | c <- [1..n], b <- [1..c], a <- [1..b], a ** 2 + b ** 2 == c ** 2]
+
+betuSzam = zip['a' .. 'z'] [0 .. 25]
+
+szamok1 = zip [0 .. 10] [5,4 .. 0]
+szamok2 n = [(i, n - i) | i <- [0 .. n]]
+szamok3 n = zip [0 .. n] [n, n-1 .. 0]
+
+--vegtelen lista
+tfLs n = take n ls
+    where 
+        ls = [True,False] ++ ls
+
+--kb ez lenne a main
+main :: IO()
+main = do
+    putStrLn "x hatvany n" 
+    print(xhatvanyN 5 3)
+    putStrLn "paros osztok"
+    print(osztokN 48)
+    putStrLn "osszetett szamok listaja"
+    print(osszetettN 48)
+    putStrLn "n-nel kisebb pitagoraszi szamharmasok"
+    print(pitagorasz 100)
+    putStrLn ("n-nel kisebb pitagoraszi szamharmasok" ++ show (pitagorasz 50))
+    print betuSzam
+    print szamok1
+    print(szamok2 10)
+    print(szamok3 10)
+    putStrLn("5 True,False" ++ show (tfLs 5))
